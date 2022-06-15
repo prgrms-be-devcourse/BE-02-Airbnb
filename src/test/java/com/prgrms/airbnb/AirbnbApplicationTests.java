b0194c8a1333945e367ec9e5c115d21598f6a5d7
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-06-15 18:37:45 +0900

    :tada: chore: 프로젝트 기본 설정

---
+++ src/test/java/com/prgrms/airbnb/AirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
