2b0dba3b2695fb0551ff808427030eb14de6525f
unnokid <kimki0609@gmail.com>
2022-06-20 18:24:56 +0900

    [FBNB-56] feat: queryDsl 관련 repository 추가

--- src/main/java/com/prgrms/airbnb/room/repository/RoomRepository.java
+++ src/main/java/com/prgrms/airbnb/room/repository/RoomRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long>, RoomCustomRepository {
 
-  Page<Room> findAll(Pageable pageable);
 }
