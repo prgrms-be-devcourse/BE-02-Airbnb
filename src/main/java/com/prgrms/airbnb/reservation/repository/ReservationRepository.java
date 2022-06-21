bcc0a2c55538d2933a39fc601fe7c12dbf353506
dhkstnaos <dhkstnaos@gmail.com>
2022-06-21 15:28:29 +0900

    [FBNB-51] feat: host 예약 신청 취소

--- src/main/java/com/prgrms/airbnb/reservation/repository/ReservationRepository.java
+++ src/main/java/com/prgrms/airbnb/reservation/repository/ReservationRepository.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public interface ReservationRepository extends JpaRepository<Reservation, String> {
 
-    default String createReservationNo() {
+    default String createReservationId() {
         int randomNo = ThreadLocalRandom.current().nextInt(900000) + 100000;
         String number = String.format("%tY%<tm%<td%<tH-%d", new Date(), randomNo);
         return number;
--- src/main/java/com/prgrms/airbnb/reservation/service/HostReservationService.java
+++ src/main/java/com/prgrms/airbnb/reservation/service/HostReservationService.java
@@ -1,6 +1,11 @@
 package com.prgrms.airbnb.reservation.service;
 
+import com.prgrms.airbnb.reservation.domain.Reservation;
+import com.prgrms.airbnb.reservation.domain.ReservationStatus;
+import com.prgrms.airbnb.reservation.dto.ReservationDetailResponseForHost;
 import com.prgrms.airbnb.reservation.repository.ReservationRepository;
+import com.prgrms.airbnb.room.repository.RoomRepository;
+import com.prgrms.airbnb.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,6 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class HostReservationService {
 
-    private ReservationRepository reservationRepository;
+    private final ReservationRepository reservationRepository;
+    private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
 
+    public HostReservationService(ReservationRepository reservationRepository, RoomRepository roomRepository, UserRepository userRepository) {
+        this.reservationRepository = reservationRepository;
+        this.roomRepository = roomRepository;
+        this.userRepository = userRepository;
+    }
+
+    public ReservationDetailResponseForHost findDetailById(String reservationId) {
+        return null;
+    }
+
+    @Transactional
+    public void cancel(String reservationId) {
+        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(IllegalArgumentException::new);
+        reservation.cancelReservation(ReservationStatus.ACCEPTED_AFTER_CANCELLED);
+    }
 }
