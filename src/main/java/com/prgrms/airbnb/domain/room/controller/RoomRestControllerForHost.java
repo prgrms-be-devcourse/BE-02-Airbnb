c645cab4f92bcdf70d5ebea2218e556df04bf044
Hyunggeun447 <bopi447@naver.com>
2022-06-23 17:50:48 +0900

    [FBNB-15,17,19] feat: 호스트는 숙소를 관리(수정, 삭제, 조회, 상세조회)할 수 있습니다.

--- src/main/java/com/prgrms/airbnb/domain/room/controller/RoomRestControllerForHost.java
+++ src/main/java/com/prgrms/airbnb/domain/room/controller/RoomRestControllerForHost.java
@@ -2,58 +2,82 @@ package com.prgrms.airbnb.domain.room.controller;
 
 import com.prgrms.airbnb.config.jwt.JwtAuthentication;
 import com.prgrms.airbnb.domain.room.dto.*;
-import com.prgrms.airbnb.domain.room.service.RoomService;
-import com.prgrms.airbnb.domain.user.entity.User;
-import com.prgrms.airbnb.domain.user.service.UserService;
-import lombok.RequiredArgsConstructor;
+import com.prgrms.airbnb.domain.room.entity.SortTypeForHost;
+import com.prgrms.airbnb.domain.room.service.RoomServiceForHost;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
-@RequestMapping("/api/v1/room")
+@RequestMapping("/api/v1/host/room")
 @RestController
-@RequiredArgsConstructor
 public class RoomRestControllerForHost {
 
-  private final RoomService roomService;
-  private final UserService userService;
+  private final RoomServiceForHost roomService;
+
+  public RoomRestControllerForHost(RoomServiceForHost roomService) {
+    this.roomService = roomService;
+  }
 
   @PostMapping
   public ResponseEntity<RoomDetailResponse> registerRoom(
       @AuthenticationPrincipal JwtAuthentication authentication,
       @RequestBody CreateRoomRequest createRoomRequest) {
-    User user = userService.findByUsername(authentication.username)
-        .orElseThrow(() -> new IllegalArgumentException(
-            "Could not found user for " + authentication.username));
-    RoomDetailResponse response = roomService.save(createRoomRequest, user);
+    RoomDetailResponse response = roomService.save(createRoomRequest, authentication.userId);
     return ResponseEntity.created(URI.create("/api/v1/room/" + response.getId()))
         .body(response);
   }
 
-//    @PutMapping
-//    // TODO: 2022/06/21 updateDto <= hostId 필요? servletRequest에 담겨져있고 아직 검증 로직이 없어서 만들어야하나
-//    public ResponseEntity<RoomDetailResponse> modifyRoom(UpdateRoomRequest updateRoomRequest,
-//                                                         HttpServletRequest request) {
-//        Long userId = (Long) request.getSession().getAttribute("userId");
-//        updateRoomRequest.setUserId(userId);
-//        RoomDetailResponse modifiedRoom = roomService.modify(updateRoomRequest);
-//
-//        return ResponseEntity.ok(modifiedRoom);
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<Page<RoomSummaryResponse>> findByHostId(HttpServletRequest request,
-//                                                                  @RequestParam("sortType") SortTypeForHost sortType,
-//                                                                  Pageable pageable) {
-//        Long hostId = (Long) request.getSession().getAttribute("userId");
-//        Page<RoomSummaryResponse> pagesHostId = roomService.findByHostId(hostId, sortType, pageable);
-//
-//        return ResponseEntity.ok(pagesHostId);
-//    }
+  @PutMapping
+  public ResponseEntity<RoomDetailResponse> modifyRoom(UpdateRoomRequest updateRoomRequest,
+      @AuthenticationPrincipal JwtAuthentication authentication) {
 
+    Long userId = authentication.userId;
+    RoomDetailResponse modifiedRoom = roomService.modify(updateRoomRequest, userId);
+
+    return ResponseEntity.ok(modifiedRoom);
+  }
+
+  @GetMapping
+  public ResponseEntity<Slice<RoomSummaryResponse>> getByHostId(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @RequestParam("sortType") SortTypeForHost sortType,
+      Pageable pageable) {
+
+    Long hostId = authentication.userId;
+    Slice<RoomSummaryResponse> pagesHostId = roomService.findByHostId(hostId, sortType, pageable);
+
+    return ResponseEntity.ok(pagesHostId);
+  }
+
+  @GetMapping("/{roomId}")
+  public ResponseEntity<RoomDetailResponse> getDetailInfo(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @PathVariable("roomId") Long roomId
+  ) {
+    RoomDetailResponse roomDetailInfo = roomService.findDetailById(roomId, authentication.userId);
+
+    return ResponseEntity.ok(roomDetailInfo);
+  }
+
+  @DeleteMapping("/{roomId}")
+  public ResponseEntity<Object> delete(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @PathVariable("roomId") Long roomId
+  ) {
+
+    roomService.remove(roomId, authentication.userId);
+    return ResponseEntity.noContent().build();
+  }
 }
--- src/main/java/com/prgrms/airbnb/domain/room/service/RoomServiceForHost.java
+++ src/main/java/com/prgrms/airbnb/domain/room/service/RoomServiceForHost.java
@@ -6,30 +6,41 @@ import com.prgrms.airbnb.domain.room.entity.SortTypeForHost;
 import com.prgrms.airbnb.domain.room.repository.RoomRepository;
 import com.prgrms.airbnb.domain.room.util.RoomConverter;
 import com.prgrms.airbnb.domain.user.entity.User;
-import lombok.RequiredArgsConstructor;
+import com.prgrms.airbnb.domain.user.repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class RoomService {
+public class RoomServiceForHost {
 
   private final RoomRepository roomRepository;
+  private final UserRepository userRepository;
+
+  public RoomServiceForHost(RoomRepository roomRepository,
+      UserRepository userRepository) {
+    this.roomRepository = roomRepository;
+    this.userRepository = userRepository;
+  }
 
   @Transactional
-  public RoomDetailResponse save(CreateRoomRequest createRoomRequest, User user) {
+  public RoomDetailResponse save(CreateRoomRequest createRoomRequest, Long hostId) {
+    User user = userRepository.findById(hostId)
+        .orElseThrow(RuntimeException::new);
     Room room = RoomConverter.toRoom(createRoomRequest, user);
     Room savedRoom = roomRepository.save(room);
     return RoomConverter.ofDetail(savedRoom);
   }
 
   @Transactional
-  public RoomDetailResponse modify(UpdateRoomRequest updateRoomRequest) {
+  public RoomDetailResponse modify(UpdateRoomRequest updateRoomRequest, Long hostId) {
     Room room = roomRepository.findById(updateRoomRequest.getId())
         .orElseThrow(IllegalArgumentException::new);
+    validateHost(room, hostId);
+
     room.changeName(updateRoomRequest.getName());
     room.changeCharge(updateRoomRequest.getCharge());
     room.getRoomInfo().changeMaxGuest(updateRoomRequest.getRoomInfo().getMaxGuest());
@@ -39,20 +50,29 @@ public class RoomService {
     return RoomConverter.ofDetail(room);
   }
 
-  public RoomDetailResponse findDetailById(Long id) {
+  public RoomDetailResponse findDetailById(Long id, Long hostId) {
     Room room = roomRepository.findById(id)
         .orElseThrow(() -> new RuntimeException("해당 id를 가진 room이 존재하지 않습니다."));
-
+    validateHost(room, hostId);
     return RoomConverter.ofDetail(room);
   }
 
-  public Page<RoomSummaryResponse> findByHostId(Long hostId, SortTypeForHost sortType,
+  public Slice<RoomSummaryResponse> findByHostId(Long hostId, SortTypeForHost sortType,
       Pageable pageable) {
     return sortType.findByHost(hostId, pageable, roomRepository)
         .map(RoomConverter::ofSummary);
   }
 
-  public void remove(Long id) {
-    roomRepository.deleteById(id);
+  public void remove(Long roomId, Long hostId) {
+    Room room = roomRepository.findById(roomId)
+        .orElseThrow(RuntimeException::new);
+    validateHost(room, hostId);
+    roomRepository.deleteById(roomId);
+  }
+
+  private void validateHost(Room room, Long hostId) {
+    if (!room.getUserId().equals(hostId)) {
+      throw new RuntimeException("잘못된 접근 [room의 host가 아님]");
+    }
   }
 }
