467746c4a3844b2e87b340bd4dd2ff5974b2d49c
unnokid <kimki0609@gmail.com>
2022-06-23 19:16:38 +0900

    [FBNB-21] feature: Review Image 추가

--- src/main/java/com/prgrms/airbnb/domain/review/service/ReviewService.java
+++ src/main/java/com/prgrms/airbnb/domain/review/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.prgrms.airbnb.domain.review.service;
 import com.prgrms.airbnb.domain.reservation.entity.Reservation;
 import com.prgrms.airbnb.domain.reservation.repository.ReservationRepository;
 import com.prgrms.airbnb.domain.review.dto.CreateReviewRequest;
+import com.prgrms.airbnb.domain.review.dto.UpdateReviewRequest;
 import com.prgrms.airbnb.domain.review.entity.Review;
 import com.prgrms.airbnb.domain.review.repository.ReviewRepository;
 import com.prgrms.airbnb.domain.review.util.ReviewConverter;
@@ -21,18 +22,24 @@ public class ReviewService {
   }
 
   public void save(String reservationId, CreateReviewRequest createReviewRequest) {
+    //TODO: 리턴 타입 지정해야함
     Reservation reservation = reservationRepository.findById(reservationId)
         .orElseThrow(IllegalArgumentException::new);
-    if (reservation.canReviewed()) {
-      //리뷰를 남길수있음
-      reviewRepository.save(new Review(createReviewRequest.getComment(),
-              createReviewRequest.getRating(),
-              reservationId,
-              createReviewRequest.getVisible()
-          )
-      );
+    if (!reservation.canReviewed()) {
+      throw new IllegalArgumentException();
     }
-
+    reviewRepository.save(ReviewConverter.toReview(reservationId, createReviewRequest));
   }
+
+  public void modify(Long reviewId, UpdateReviewRequest updateReviewRequest) {
+    //TODO: 리턴 타입 지정해야함
+    Review review = reviewRepository.findById(reviewId).orElseThrow(IllegalArgumentException::new);
+
+    review.changeComment(updateReviewRequest.getComment());
+    review.changeRating(updateReviewRequest.getRating());
+    review.changeVisible(updateReviewRequest.getVisible());
+    review.changeImage(updateReviewRequest.getImages());
+  }
+
 
 }
