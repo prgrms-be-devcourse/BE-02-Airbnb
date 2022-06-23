6f1cd700a74c43d0bb6cf52cce6c56457c39ba21
unnokid <kimki0609@gmail.com>
2022-06-23 17:25:51 +0900

    [FBNB-51] feature: Review dto 추가

---
+++ src/main/java/com/prgrms/airbnb/domain/review/dto/CreateReviewRequest.java
@@ -0,0 +1,17 @@
+package com.prgrms.airbnb.domain.review.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateReviewRequest {
+  private String comment;
+  private Integer rating;
+  private Boolean visible;
+
+}
