bf49063e9987fe1526c9e546cd750491894b5b62
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-06-23 15:27:21 +0900

    fix: token 유무 확인 추가

--- src/main/java/com/prgrms/airbnb/config/jwt/JwtAuthentication.java
+++ src/main/java/com/prgrms/airbnb/config/jwt/JwtAuthentication.java
@@ -2,6 +2,7 @@ package com.prgrms.airbnb.config.jwt;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.commons.lang3.StringUtils;
 
 // 인증 완료후 사용자를 표현하기 위한 객체 -> principal에 들어갈 객체
 public class JwtAuthentication {
@@ -10,6 +11,14 @@ public class JwtAuthentication {
   public final String username;
 
   JwtAuthentication(String token, String username) {
+    if (StringUtils.isEmpty(token)) {
+      throw new IllegalArgumentException();
+    }
+
+    if (StringUtils.isEmpty(username)) {
+      throw new IllegalArgumentException();
+    }
+
     this.token = token;
     this.username = username;
   }
