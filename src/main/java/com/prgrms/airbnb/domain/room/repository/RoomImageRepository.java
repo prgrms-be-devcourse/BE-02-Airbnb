a918ca77b3cf76d87c91cdb35e89dfd04247c292
Hyunggeun447 <bopi447@naver.com>
2022-06-24 20:47:32 +0900

    [FBNB-14] feat: RoomImageRepo 를 생성합니다.

---
+++ src/main/java/com/prgrms/airbnb/domain/room/repository/RoomImageRepository.java
@@ -0,0 +1,8 @@
+package com.prgrms.airbnb.domain.room.repository;
+
+import com.prgrms.airbnb.domain.room.entity.RoomImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
+
+}
