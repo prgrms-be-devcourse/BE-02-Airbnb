5e5388962542d77c34d842fb03bda749885ae16b
dhkstnaos <dhkstnaos@gmail.com>
2022-06-24 13:59:47 +0900

    [FBNB-21] refactor: ReviewService 수정

--- src/main/java/com/prgrms/airbnb/domain/review/service/ReviewService.java
+++ src/main/java/com/prgrms/airbnb/domain/review/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.prgrms.airbnb.domain.review.service;
 import com.prgrms.airbnb.domain.reservation.entity.Reservation;
 import com.prgrms.airbnb.domain.reservation.repository.ReservationRepository;
 import com.prgrms.airbnb.domain.review.dto.CreateReviewRequest;
+import com.prgrms.airbnb.domain.review.dto.ReviewResponse;
 import com.prgrms.airbnb.domain.review.dto.UpdateReviewRequest;
 import com.prgrms.airbnb.domain.review.entity.Review;
 import com.prgrms.airbnb.domain.review.repository.ReviewRepository;
@@ -14,36 +15,38 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ReviewService {
 
-  private final ReviewRepository reviewRepository;
-  private final ReservationRepository reservationRepository;
+    private final ReviewRepository reviewRepository;
+    private final ReservationRepository reservationRepository;
 
-  public ReviewService(ReviewRepository reviewRepository,
-      ReservationRepository reservationRepository) {
-    this.reviewRepository = reviewRepository;
-    this.reservationRepository = reservationRepository;
-  }
-
-  @Transactional
-  public void save(String reservationId, CreateReviewRequest createReviewRequest) {
-    //TODO: 리턴 타입 지정해야함
-    Reservation reservation = reservationRepository.findById(reservationId)
-        .orElseThrow(IllegalArgumentException::new);
-    if (!reservation.canReviewed()) {
-      throw new IllegalArgumentException();
+    public ReviewService(ReviewRepository reviewRepository,
+                         ReservationRepository reservationRepository) {
+        this.reviewRepository = reviewRepository;
+        this.reservationRepository = reservationRepository;
     }
-    reviewRepository.save(ReviewConverter.toReview(reservationId, createReviewRequest));
-  }
 
-  @Transactional
-  public void modify(Long reviewId, UpdateReviewRequest updateReviewRequest) {
-    //TODO: 리턴 타입 지정해야함
-    Review review = reviewRepository.findById(reviewId).orElseThrow(IllegalArgumentException::new);
+    @Transactional
+    public ReviewResponse save(String reservationId, CreateReviewRequest createReviewRequest) {
+        //TODO: 리턴 타입 지정해야함
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(IllegalArgumentException::new);
+        if (!reservation.canReviewed()) {
+            throw new IllegalArgumentException();
+        }
+        Review review = ReviewConverter.toReview(reservationId, createReviewRequest);
+        Review savedReview = reviewRepository.save(review);
+        return ReviewConverter.of(savedReview);
+    }
 
-    review.changeComment(updateReviewRequest.getComment());
-    review.changeRating(updateReviewRequest.getRating());
-    review.changeVisible(updateReviewRequest.getVisible());
-    review.changeImage(updateReviewRequest.getImages());
-  }
+    @Transactional
+    public ReviewResponse modify(Long reviewId, UpdateReviewRequest updateReviewRequest) {
+        //TODO: 리턴 타입 지정해야함
+        Review review = reviewRepository.findById(reviewId).orElseThrow(IllegalArgumentException::new);
+        review.changeComment(updateReviewRequest.getComment());
+        review.changeRating(updateReviewRequest.getRating());
+        review.changeVisible(updateReviewRequest.getVisible());
+        review.changeImage(updateReviewRequest.getImages());
+        return ReviewConverter.of(review);
+    }
 
 
 }
