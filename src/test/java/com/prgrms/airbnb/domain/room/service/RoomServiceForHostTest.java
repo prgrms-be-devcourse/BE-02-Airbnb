0d0af94d4334be84b1deccf9c82be3a001a0c1e4
Hyunggeun447 <bopi447@naver.com>
2022-06-24 21:12:53 +0900

    [FBNB-79] feat: RoomServiceForHostTest 생성 관련 테스트를 추가합니다..

--- src/test/java/com/prgrms/airbnb/domain/room/service/RoomServiceForHostTest.java
+++ src/test/java/com/prgrms/airbnb/domain/room/service/RoomServiceForHostTest.java
@@ -1,7 +1,6 @@
 package com.prgrms.airbnb.domain.room.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import com.prgrms.airbnb.domain.common.entity.Address;
 import com.prgrms.airbnb.domain.common.entity.Email;
@@ -13,14 +12,14 @@ import com.prgrms.airbnb.domain.room.entity.RoomInfo;
 import com.prgrms.airbnb.domain.room.entity.RoomType;
 import com.prgrms.airbnb.domain.room.repository.RoomImageRepository;
 import com.prgrms.airbnb.domain.room.repository.RoomRepository;
-import com.prgrms.airbnb.domain.room.util.RoomConverter;
 import com.prgrms.airbnb.domain.user.entity.Group;
 import com.prgrms.airbnb.domain.user.entity.User;
 import com.prgrms.airbnb.domain.user.repository.GroupRepository;
 import com.prgrms.airbnb.domain.user.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -54,12 +53,12 @@ class RoomServiceForHostTest {
         .orElseThrow(() -> new IllegalStateException("Could not found group for USER_GROUP"));
 
     User user = userRepository.save(new User("user",
-        "pr",
-        "prId",
-        "proim",
+        "provider",
+        "providerId",
+        "profileImage",
         group,
         new Email("aaa@gmail.com")));
-    id = user.getId();
+    hostId = user.getId();
 
     Address address = new Address("default address1", "default address2");
     Integer charge = 10000;
@@ -75,7 +74,6 @@ class RoomServiceForHostTest {
     images.add(roomImage3);
     RoomImage roomImage4 = new RoomImage("default roomImage Path 4");
     images.add(roomImage4);
-    Long hostId = 1000L;
 
     Room room = new Room(address, charge, roomName, roomDescription, roomInfo, RoomType.APARTMENT,
         images, hostId);
@@ -83,14 +81,21 @@ class RoomServiceForHostTest {
     defaultRoom = roomRepository.save(room);
   }
 
+  @AfterEach
+  void cleanUp() {
+    roomImageRepository.deleteAll();
+    userRepository.deleteAll();
+    roomRepository.deleteAll();
+  }
+
   Room defaultRoom;
-  Long id;
+  Long hostId;
 
   @Nested
   class 저장 {
 
     @Test
-    @DisplayName("성공: room 저장에 성공합니다.")
+    @DisplayName("성공: room 저장에 성공합니다. roomImage도 관련 repo에 저장됩니다.")
     public void success() throws Exception {
 
       //given
@@ -108,7 +113,6 @@ class RoomServiceForHostTest {
       images.add(roomImage3);
       RoomImage roomImage4 = new RoomImage("roomImage Path 4");
       images.add(roomImage4);
-      Long hostId = id;
 
       //when
       CreateRoomRequest createRoomRequest = CreateRoomRequest.builder()
@@ -135,7 +139,156 @@ class RoomServiceForHostTest {
       assertThat(createRoomResponse.getImages()).contains(roomImage3);
       assertThat(createRoomResponse.getImages()).contains(roomImage4);
       assertThat(createRoomResponse.getUserId()).isEqualTo(hostId);
+
+      List<RoomImage> allRoomImages = roomImageRepository.findAll();
+
+      assertThat(allRoomImages).contains(roomImage1);
+      assertThat(allRoomImages).contains(roomImage2);
+      assertThat(allRoomImages).contains(roomImage3);
+      assertThat(allRoomImages).contains(roomImage4);
     }
+
+    @Test
+    @DisplayName("성공: room 저장에 성공합니다. roomImage가 없다면 repo역시 저장되지 않습니다.")
+    public void successWithoutImage() throws Exception {
+
+      //given
+      Address address = new Address("address1", "address2");
+      Integer charge = 10000;
+      String roomName = "roomName";
+      String roomDescription = "roomDescription";
+      RoomInfo roomInfo = new RoomInfo(1, 1, 1, 1);
+      List<RoomImage> images = new ArrayList<>();
+
+      RoomImage roomImage1 = new RoomImage("roomImage Path 1");
+      RoomImage roomImage2 = new RoomImage("roomImage Path 2");
+      RoomImage roomImage3 = new RoomImage("roomImage Path 3");
+      RoomImage roomImage4 = new RoomImage("roomImage Path 4");
+
+      //when
+      CreateRoomRequest createRoomRequest = CreateRoomRequest.builder()
+          .address(address)
+          .charge(charge)
+          .name(roomName)
+          .description(roomDescription)
+          .roomInfo(roomInfo)
+          .roomType(RoomType.APARTMENT)
+          .build();
+
+      RoomDetailResponse createRoomResponse = roomServiceForHost.save(createRoomRequest, hostId);
+
+      //then
+      assertThat(createRoomResponse.getId()).isNotNull();
+      assertThat(createRoomResponse.getAddress()).isEqualTo(address);
+      assertThat(createRoomResponse.getCharge()).isEqualTo(charge);
+      assertThat(createRoomResponse.getName()).isEqualTo(roomName);
+      assertThat(createRoomResponse.getDescription()).isEqualTo(roomDescription);
+      assertThat(createRoomResponse.getRoomInfo()).isEqualTo(roomInfo);
+      assertThat(createRoomResponse.getImages()).doesNotContain(roomImage1);
+      assertThat(createRoomResponse.getImages()).doesNotContain(roomImage2);
+      assertThat(createRoomResponse.getImages()).doesNotContain(roomImage3);
+      assertThat(createRoomResponse.getImages()).doesNotContain(roomImage4);
+      assertThat(createRoomResponse.getUserId()).isEqualTo(hostId);
+
+      List<RoomImage> allRoomImages = roomImageRepository.findAll();
+
+      assertThat(allRoomImages).doesNotContain(roomImage1);
+      assertThat(allRoomImages).doesNotContain(roomImage2);
+      assertThat(allRoomImages).doesNotContain(roomImage3);
+      assertThat(allRoomImages).doesNotContain(roomImage4);
+    }
+
+    @Test
+    @DisplayName("실패: DB에 같은 주소의 Room이 존재한다면 새 Room 을 등록할 수 없습니다.")
+    public void failCreateSameAddressRoom() throws Exception {
+
+      //given
+      Integer charge = 10000;
+      String roomName = "new roomName";
+      String roomDescription = "new roomDescription";
+      RoomInfo roomInfo = new RoomInfo(1, 1, 1, 1);
+      List<RoomImage> images = new ArrayList<>();
+      RoomImage roomImage1 = new RoomImage("new roomImage Path 1");
+      images.add(roomImage1);
+      RoomImage roomImage2 = new RoomImage("new roomImage Path 2");
+      images.add(roomImage2);
+      RoomImage roomImage3 = new RoomImage("new roomImage Path 3");
+      images.add(roomImage3);
+      RoomImage roomImage4 = new RoomImage("new roomImage Path 4");
+      images.add(roomImage4);
+
+      //when
+      Address address = new Address("default address1", "default address2");
+
+      CreateRoomRequest createRoomRequest = CreateRoomRequest.builder()
+          .address(address)
+          .charge(charge)
+          .name(roomName)
+          .description(roomDescription)
+          .roomInfo(roomInfo)
+          .roomType(RoomType.APARTMENT)
+          .roomImages(images)
+          .build();
+
+      //then
+      Assertions.assertThrows(RuntimeException.class,
+          () -> roomServiceForHost.save(createRoomRequest, hostId));
+    }
+
+    @Test
+    @DisplayName("성공: 주소를 제외한 어떠한 값도 중복될 수 있습니다.")
+    public void success3() throws Exception {
+
+      //given
+      Integer charge = 10000;
+      String roomName = "default roomName";
+      String roomDescription = "default roomDescription";
+      RoomInfo roomInfo = new RoomInfo(1, 1, 1, 1);
+      List<RoomImage> images = new ArrayList<>();
+      RoomImage roomImage1 = new RoomImage("default roomImage Path 1");
+      images.add(roomImage1);
+      RoomImage roomImage2 = new RoomImage("default roomImage Path 2");
+      images.add(roomImage2);
+      RoomImage roomImage3 = new RoomImage("default roomImage Path 3");
+      images.add(roomImage3);
+      RoomImage roomImage4 = new RoomImage("default roomImage Path 4");
+      images.add(roomImage4);
+
+      //when
+      Address address = new Address("address1", "address2");
+      CreateRoomRequest createRoomRequest = CreateRoomRequest.builder()
+          .address(address)
+          .charge(charge)
+          .name(roomName)
+          .description(roomDescription)
+          .roomInfo(roomInfo)
+          .roomType(RoomType.APARTMENT)
+          .roomImages(images)
+          .build();
+
+      RoomDetailResponse createRoomResponse = roomServiceForHost.save(createRoomRequest, hostId);
+
+      //then
+      assertThat(createRoomResponse.getId()).isNotNull();
+      assertThat(createRoomResponse.getAddress()).isEqualTo(address);
+      assertThat(createRoomResponse.getCharge()).isEqualTo(charge);
+      assertThat(createRoomResponse.getName()).isEqualTo(roomName);
+      assertThat(createRoomResponse.getDescription()).isEqualTo(roomDescription);
+      assertThat(createRoomResponse.getRoomInfo()).isEqualTo(roomInfo);
+      assertThat(createRoomResponse.getImages()).contains(roomImage1);
+      assertThat(createRoomResponse.getImages()).contains(roomImage2);
+      assertThat(createRoomResponse.getImages()).contains(roomImage3);
+      assertThat(createRoomResponse.getImages()).contains(roomImage4);
+      assertThat(createRoomResponse.getUserId()).isEqualTo(hostId);
+
+      List<RoomImage> allRoomImages = roomImageRepository.findAll();
+
+      assertThat(allRoomImages).contains(roomImage1);
+      assertThat(allRoomImages).contains(roomImage2);
+      assertThat(allRoomImages).contains(roomImage3);
+      assertThat(allRoomImages).contains(roomImage4);
+    }
+
 
   }
 
