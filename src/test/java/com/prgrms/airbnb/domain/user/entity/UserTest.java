99b219c844dea1db92678595e97c50665506dc7a
Hyunggeun447 <bopi447@naver.com>
2022-06-24 17:39:42 +0900

    [FBNB-71] refactoring: UserTest공백 제거

--- src/test/java/com/prgrms/airbnb/domain/user/entity/UserTest.java
+++ src/test/java/com/prgrms/airbnb/domain/user/entity/UserTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.prgrms.airbnb.domain.common.entity.Email;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -218,15 +216,5 @@ class UserTest {
               null,
               null));
     }
-
-
-
   }
-
-
-
-
-
-
-
 }
