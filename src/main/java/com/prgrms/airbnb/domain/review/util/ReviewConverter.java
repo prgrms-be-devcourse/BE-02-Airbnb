8cb4f7c269af76fbfec6570cabde02a72e4bcc75
dhkstnaos <dhkstnaos@gmail.com>
2022-06-24 13:57:44 +0900

    [FBNB-21] feat: ReviewResponse Converter 생성

--- src/main/java/com/prgrms/airbnb/domain/review/util/ReviewConverter.java
+++ src/main/java/com/prgrms/airbnb/domain/review/util/ReviewConverter.java
@@ -1,19 +1,30 @@
 package com.prgrms.airbnb.domain.review.util;
 
 import com.prgrms.airbnb.domain.review.dto.CreateReviewRequest;
+import com.prgrms.airbnb.domain.review.dto.ReviewResponse;
 import com.prgrms.airbnb.domain.review.entity.Review;
 
 public class ReviewConverter {
 
-  public static Review toReview(String reservationId,CreateReviewRequest createReviewRequest){
-    return new Review(
-        createReviewRequest.getComment(),
-        createReviewRequest.getRating(),
-        reservationId,
-        createReviewRequest.getVisible(),
-        createReviewRequest.getImages()
-    );
-  }
+    public static Review toReview(String reservationId, CreateReviewRequest createReviewRequest) {
+        return new Review(
+                createReviewRequest.getComment(),
+                createReviewRequest.getRating(),
+                reservationId,
+                createReviewRequest.getVisible(),
+                createReviewRequest.getImages()
+        );
+    }
+
+    public static ReviewResponse of(Review review) {
+        return ReviewResponse.builder()
+                .id(review.getId())
+                .comment(review.getComment())
+                .rating(review.getRating())
+                .visible(review.getVisible())
+                .images(review.getImages())
+                .build();
+    }
 
 
 }
