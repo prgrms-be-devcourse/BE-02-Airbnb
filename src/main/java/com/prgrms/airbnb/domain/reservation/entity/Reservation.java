0b9bdab80de1b5b89885d27869491c02b483eeaa
unnokid <kimki0609@gmail.com>
2022-06-27 17:24:00 +0900

    [FBNB-36] feat: Reservation 추가 검증 조건 추가

--- src/main/java/com/prgrms/airbnb/domain/reservation/entity/Reservation.java
+++ src/main/java/com/prgrms/airbnb/domain/reservation/entity/Reservation.java
@@ -16,115 +16,126 @@ import java.time.LocalDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Reservation extends BaseEntity {
 
-    @Id
-    @Column(name = "id")
-    private String id;
+  @Id
+  @Column(name = "id")
+  private String id;
 
-    @Enumerated(value = EnumType.STRING)
-    @Column(name = "reservation_status")
-    private ReservationStatus reservationStatus = ReservationStatus.WAITED_OK;
+  @Enumerated(value = EnumType.STRING)
+  @Column(name = "reservation_status")
+  private ReservationStatus reservationStatus = ReservationStatus.WAITED_OK;
 
-    @Column(name = "start_date")
-    private LocalDate startDate;
+  @Column(name = "start_date")
+  private LocalDate startDate;
 
-    @Column(name = "end_date")
-    private LocalDate endDate;
+  @Column(name = "end_date")
+  private LocalDate endDate;
 
-    @Column(name = "term")
-    private Integer term;
+  @Column(name = "term")
+  private Integer term;
 
-    @Column(name = "total_price")
-    private Integer totalPrice;
+  @Column(name = "total_price")
+  private Integer totalPrice;
 
-    @NotNull(message = "userId는 null 일 수 없습니다")
-    @Column(name = "user_id")
-    private Long userId;
+  @NotNull(message = "userId는 null 일 수 없습니다")
+  @Column(name = "user_id")
+  private Long userId;
 
-    @NotNull(message = "roomId는 null 일 수 없습니다")
-    @Column(name = "room_id")
-    private Long roomId;
+  @NotNull(message = "roomId는 null 일 수 없습니다")
+  @Column(name = "room_id")
+  private Long roomId;
 
-    public Reservation(String id, ReservationStatus reservationStatus, LocalDate startDate,
-                       LocalDate endDate, Integer term, Integer oneDayCharge, Long userId, Long roomId) {
-        setId(id);
-        setReservationStatus(reservationStatus);
-        setStartDate(startDate);
-        setEndDate(endDate);
-        setTerm(term);
-        calculatePrice(oneDayCharge);
-        setUserId(userId);
-        setRoomId(roomId);
+  public Reservation(String id, ReservationStatus reservationStatus, LocalDate startDate,
+      LocalDate endDate, Integer term, Integer oneDayCharge, Long userId, Long roomId) {
+    setId(id);
+    setReservationStatus(reservationStatus);
+    setStartDate(startDate);
+    setEndDate(endDate);
+    checkDate(startDate, endDate);
+    setTerm(term);
+    calculatePrice(oneDayCharge);
+    setUserId(userId);
+    setRoomId(roomId);
+  }
+
+  public void changeStatus(ReservationStatus newReservationStatus) {
+    reservationStatus = reservationStatus.changeStatus(newReservationStatus);
+  }
+
+  public boolean canReviewed() {
+    if (endDate.plusDays(14).isBefore(LocalDate.now())) {
+      return false;
     }
+    return reservationStatus.equals(ReservationStatus.WAIT_REVIEW);
+  }
 
-    public void changeStatus(ReservationStatus newReservationStatus) {
-        reservationStatus = reservationStatus.changeStatus(newReservationStatus);
+  public boolean canCancelled() {
+    if (startDate.isAfter(LocalDate.now()) || startDate.isEqual(LocalDate.now())) {
+      //TODO: 환불 정책 필요, 에러 추가 필요
+      throw new IllegalArgumentException();
     }
+    return this.reservationStatus.equals(ReservationStatus.WAITED_OK)
+        || this.reservationStatus.equals(ReservationStatus.ACCEPTED);
+  }
 
-    public boolean canReviewed() {
-        if (endDate.plusDays(14).isBefore(LocalDate.now())) {
-            return false;
-        }
-        return reservationStatus.equals(ReservationStatus.WAIT_REVIEW);
+  private void setId(String id) {
+    if (ObjectUtils.isEmpty(id)) {
+      throw new IllegalArgumentException();
     }
+    this.id = id;
+  }
 
-    public boolean canCancelled() {
-        if (startDate.isAfter(LocalDate.now()) || startDate.isEqual(LocalDate.now())) {
-            //TODO: 환불 정책 필요, 에러 추가 필요
-            throw new IllegalArgumentException();
-        }
-        return this.reservationStatus.equals(ReservationStatus.WAITED_OK)
-                || this.reservationStatus.equals(ReservationStatus.ACCEPTED);
+  private void setReservationStatus(ReservationStatus reservationStatus) {
+    if (ObjectUtils.isEmpty(reservationStatus) || !(reservationStatus.equals(
+        ReservationStatus.WAITED_OK))) {
+      throw new IllegalArgumentException();
     }
+  }
 
-    private void setId(String id) {
-        if (ObjectUtils.isEmpty(id)) {
-            throw new IllegalArgumentException();
-        }
-        this.id = id;
+  private void setStartDate(LocalDate checkIn) {
+    if (ObjectUtils.isEmpty(checkIn)) {
+      throw new IllegalArgumentException();
     }
+    this.startDate = checkIn;
+  }
 
-    private void setReservationStatus(ReservationStatus reservationStatus) {
-        if (ObjectUtils.isEmpty(reservationStatus)) {
-            throw new IllegalArgumentException();
-        }
+  private void setEndDate(LocalDate checkOut) {
+    if (ObjectUtils.isEmpty(checkOut)) {
+      throw new IllegalArgumentException();
     }
+    this.endDate = checkOut;
+  }
 
-    private void setStartDate(LocalDate checkIn) {
-        if (ObjectUtils.isEmpty(checkIn)) {
-            throw new IllegalArgumentException();
-        }
-        this.startDate = checkIn;
+  private void setTerm(Integer period) {
+    if (ObjectUtils.isEmpty(period) || period <= 0) {
+      throw new IllegalArgumentException();
     }
+    this.term = period;
+  }
 
-    private void setEndDate(LocalDate checkOut) {
-        if (ObjectUtils.isEmpty(checkOut)) {
-            throw new IllegalArgumentException();
-        }
-        this.endDate = checkOut;
+  private void setUserId(Long userId) {
+    if (ObjectUtils.isEmpty(userId)) {
+      throw new IllegalArgumentException();
     }
+    this.userId = userId;
+  }
 
-    private void setTerm(Integer period) {
-        if (ObjectUtils.isEmpty(period)) {
-            throw new IllegalArgumentException();
-        }
-        this.term = period;
+  private void setRoomId(Long roomId) {
+    if (ObjectUtils.isEmpty(roomId)) {
+      throw new IllegalArgumentException();
     }
+    this.roomId = roomId;
+  }
 
-    private void setUserId(Long userId) {
-        if (ObjectUtils.isEmpty(userId)) {
-            throw new IllegalArgumentException();
-        }
-        this.userId = userId;
+  private void calculatePrice(Integer charge) {
+    if (ObjectUtils.isEmpty(charge) || charge < 0) {
+      throw new IllegalArgumentException();
     }
+    totalPrice = charge * term;
+  }
 
-    private void setRoomId(Long roomId) {
-        if (ObjectUtils.isEmpty(roomId)) {
-            throw new IllegalArgumentException();
-        }
-        this.roomId = roomId;
+  private void checkDate(LocalDate startDate, LocalDate endDate) {
+    if (startDate.isAfter(endDate)) {
+      throw new IllegalArgumentException();
     }
-
-    private void calculatePrice(Integer charge) {
-        totalPrice = charge * term;
-    }
+  }
 }
