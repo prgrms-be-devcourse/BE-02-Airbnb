e9d7817d6b9c22ade5452dd871f510261e49b681
Hyunggeun447 <bopi447@naver.com>
2022-06-30 14:25:20 +0900

    :fire: [FBNB-79] Hot-FIx: build 깨지는 문제 및 dto의 setter 원복

--- src/test/java/com/prgrms/airbnb/domain/room/service/RoomServiceForHostTest.java
+++ src/test/java/com/prgrms/airbnb/domain/room/service/RoomServiceForHostTest.java
@@ -32,7 +32,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
@@ -41,7 +40,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @Transactional
 class RoomServiceForHostTest {
 
