4a9fbb37cbc123cc14d7d02f601e91239e3f498f
unnokid <kimki0609@gmail.com>
2022-07-01 18:28:21 +0900

    [FBNB-96] refactoring: 사용되지 않는 메소드 제거

--- src/main/java/com/prgrms/airbnb/domain/reservation/entity/Reservation.java
+++ src/main/java/com/prgrms/airbnb/domain/reservation/entity/Reservation.java
@@ -72,10 +72,6 @@ public class Reservation extends BaseEntity {
     return reservationStatus.equals(ReservationStatus.WAIT_REVIEW);
   }
 
-  public boolean canReservation(LocalDate newStartDate, LocalDate newEndDate) {
-    return validateDateForReservation(newStartDate, newEndDate);
-  }
-
   public boolean canCancelled() {
     if (!validateDateForCancel()) {
       //TODO: 환불 정책 필요
@@ -153,9 +149,5 @@ public class Reservation extends BaseEntity {
 
   private boolean validateDateForReview() {
     return endDate.plusDays(14).isBefore(LocalDate.now());
-  }
-
-  private boolean validateDateForReservation(LocalDate newStartDate, LocalDate newEndDate) {
-    return endDate.isBefore(newStartDate) || newEndDate.isBefore(startDate);
   }
 }
