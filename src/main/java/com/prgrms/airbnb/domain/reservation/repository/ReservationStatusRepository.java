e8a1817f753909c4e0abe875afc4e44b921c060e
unnokid <kimki0609@gmail.com>
2022-07-01 18:29:33 +0900

    [FBNB-96] feat: existReservation 메소드 추가

--- src/main/java/com/prgrms/airbnb/domain/reservation/repository/ReservationStatusRepository.java
+++ src/main/java/com/prgrms/airbnb/domain/reservation/repository/ReservationStatusRepository.java
@@ -1,10 +1,12 @@
 package com.prgrms.airbnb.domain.reservation.repository;
 
 import com.prgrms.airbnb.domain.reservation.entity.Reservation;
+import java.time.LocalDate;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface ReservationStatusRepository {
     void updateReservationStatus();
     Slice<Reservation> findAllReservationByHostId(Long hostId, Pageable pageable);
+    boolean existReservation(Long roomId, LocalDate startDate, LocalDate endDate);
 }
