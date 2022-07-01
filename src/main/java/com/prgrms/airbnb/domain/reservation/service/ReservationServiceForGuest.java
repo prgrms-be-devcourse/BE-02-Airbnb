7ca592d69007fd1c8ed0176f8638f9a6ea68177c
unnokid <kimki0609@gmail.com>
2022-07-01 18:32:27 +0900

    [FBNB-96] refactoring: save 메소드 변경

--- src/main/java/com/prgrms/airbnb/domain/reservation/service/ReservationServiceForGuest.java
+++ src/main/java/com/prgrms/airbnb/domain/reservation/service/ReservationServiceForGuest.java
@@ -56,13 +56,9 @@ public class ReservationServiceForGuest {
       throw new IllegalArgumentException();
     }
     //TODO: 저장하기전에 앞서 예약이 존재하는지 확인해야함
-    if (reservationRepository.findAll()
-        .stream()
-        .anyMatch(v ->
-            !v.canReservation(
-                createReservationRequest.getStartDate(),
-                createReservationRequest.getEndDate())
-        )) {
+    if (reservationRepository.existReservation(room.getId(),
+        createReservationRequest.getStartDate(),
+        createReservationRequest.getEndDate())) {
       throw new IllegalArgumentException();
     }
     String reservationId = reservationRepository.createReservationId();
