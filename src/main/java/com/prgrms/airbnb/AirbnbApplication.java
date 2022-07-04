f993a18c2c4462a8efde49ab6d600f63a6bd971e
dhkstnaos <dhkstnaos@gmail.com>
2022-07-04 16:44:14 +0900

    [FBNB-99] feat: room 리뷰 정보 변경 이벤트 생성

--- src/main/java/com/prgrms/airbnb/AirbnbApplication.java
+++ src/main/java/com/prgrms/airbnb/AirbnbApplication.java
@@ -2,7 +2,9 @@ package com.prgrms.airbnb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class AirbnbApplication {
 
