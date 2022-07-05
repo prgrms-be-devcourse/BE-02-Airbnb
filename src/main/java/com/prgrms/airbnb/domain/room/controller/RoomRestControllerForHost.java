ca3f50cd653b83a5405b571634f3156916ab6b82
Hyunggeun447 <bopi447@naver.com>
2022-07-05 17:26:21 +0900

    [FBNB-116] refactoring: 코드 컨벤션 원복

--- src/main/java/com/prgrms/airbnb/domain/room/controller/RoomRestControllerForHost.java
+++ src/main/java/com/prgrms/airbnb/domain/room/controller/RoomRestControllerForHost.java
@@ -8,6 +8,7 @@ import com.prgrms.airbnb.domain.room.dto.UpdateRoomRequest;
 import com.prgrms.airbnb.domain.room.entity.SortTypeForHost;
 import com.prgrms.airbnb.domain.room.service.RoomServiceForHost;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
@@ -22,72 +23,72 @@ import java.util.List;
 @RestController
 public class RoomRestControllerForHost {
 
-    private final RoomServiceForHost roomService;
+  private final RoomServiceForHost roomService;
 
-    public RoomRestControllerForHost(RoomServiceForHost roomService) {
-        this.roomService = roomService;
-    }
+  public RoomRestControllerForHost(RoomServiceForHost roomService) {
+    this.roomService = roomService;
+  }
 
-    @PostMapping
-    @ApiOperation(value = "Room 1개 등록", notes = "호스트는 룸 1개를 사진들과 함께 등록할 수 있다.")
-    public ResponseEntity<RoomDetailResponse> registerRoom(
-            @AuthenticationPrincipal JwtAuthentication authentication,
-            @RequestPart(value = "room") CreateRoomRequest createRoomRequest,
-            @RequestPart(value = "file", required = false) List<MultipartFile> multipartFiles) {
+  @PostMapping
+  @ApiOperation(value = "Room 1개 등록", notes = "호스트는 룸 1개를 사진들과 함께 등록할 수 있다.")
+  public ResponseEntity<RoomDetailResponse> registerRoom(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @ApiParam(value = "room") @RequestPart(value = "room") CreateRoomRequest createRoomRequest,
+      @ApiParam(value = "file") @RequestPart(value = "file", required = false) List<MultipartFile> multipartFiles) {
 
-        RoomDetailResponse response
-            = roomService.save(createRoomRequest, multipartFiles, authentication.userId);
+    RoomDetailResponse response
+        = roomService.save(createRoomRequest, multipartFiles, authentication.userId);
 
-        return ResponseEntity.created(URI.create("/api/v1/room/" + response.getId()))
-                .body(response);
-    }
+    return ResponseEntity.created(URI.create("/api/v1/room/" + response.getId()))
+        .body(response);
+  }
 
-    @PutMapping
-    @ApiOperation(value = "Room의 정보 수정", notes = "호스트는 본인의 룸 정보를 수정할 수 있다.")
-    public ResponseEntity<RoomDetailResponse> modifyRoom(
-            @RequestPart(value = "room") UpdateRoomRequest updateRoomRequest,
-            @RequestPart(value = "file", required = false) List<MultipartFile> multipartFiles,
-            @AuthenticationPrincipal JwtAuthentication authentication) {
+  @PutMapping
+  @ApiOperation(value = "Room의 정보 수정", notes = "호스트는 본인의 룸 정보를 수정할 수 있다.")
+  public ResponseEntity<RoomDetailResponse> modifyRoom(
+      @ApiParam(value = "room") @RequestPart(value = "room") UpdateRoomRequest updateRoomRequest,
+      @ApiParam(value = "room") @RequestPart(value = "file", required = false) List<MultipartFile> multipartFiles,
+      @ApiParam(value = "room") @AuthenticationPrincipal JwtAuthentication authentication) {
 
-        Long userId = authentication.userId;
-        RoomDetailResponse modifiedRoom
-            = roomService.modify(updateRoomRequest, multipartFiles, userId);
+    Long userId = authentication.userId;
+    RoomDetailResponse modifiedRoom
+        = roomService.modify(updateRoomRequest, multipartFiles, userId);
 
-        return ResponseEntity.ok(modifiedRoom);
-    }
+    return ResponseEntity.ok(modifiedRoom);
+  }
 
-    @GetMapping
-    @ApiOperation(value = "Host의 전체 Room 목록 조회", notes = "호스트는 본인의 Room 목록을 조회할 수 있다.")
-    public ResponseEntity<Slice<RoomSummaryResponse>> getByHostId(
-            @AuthenticationPrincipal JwtAuthentication authentication,
-            @RequestParam("sortType") SortTypeForHost sortType,
-            Pageable pageable) {
+  @GetMapping
+  @ApiOperation(value = "Host의 전체 Room 목록 조회", notes = "호스트는 본인의 Room 목록을 조회할 수 있다.")
+  public ResponseEntity<Slice<RoomSummaryResponse>> getByHostId(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @RequestParam("sortType") SortTypeForHost sortType,
+      Pageable pageable) {
 
-        Long hostId = authentication.userId;
-        Slice<RoomSummaryResponse> pagesHostId
-            = roomService.findByHostId(hostId, sortType, pageable);
+    Long hostId = authentication.userId;
+    Slice<RoomSummaryResponse> pagesHostId
+        = roomService.findByHostId(hostId, sortType, pageable);
 
-        return ResponseEntity.ok(pagesHostId);
-    }
+    return ResponseEntity.ok(pagesHostId);
+  }
 
-    @GetMapping("/{roomId}")
-    @ApiOperation(value = "Room 1개 상세정보 조회", notes = "호스트는 본인의 특정 Room 1개의 상세 정보를 조회할 수 있다.")
-    public ResponseEntity<RoomDetailResponse> getDetail(
-            @AuthenticationPrincipal JwtAuthentication authentication,
-            @PathVariable("roomId") Long roomId) {
+  @GetMapping("/{roomId}")
+  @ApiOperation(value = "Room 1개 상세정보 조회", notes = "호스트는 본인의 특정 Room 1개의 상세 정보를 조회할 수 있다.")
+  public ResponseEntity<RoomDetailResponse> getDetail(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @PathVariable("roomId") Long roomId) {
 
-        RoomDetailResponse roomDetailInfo
-            = roomService.findDetailById(roomId, authentication.userId);
-        return ResponseEntity.ok(roomDetailInfo);
-    }
+    RoomDetailResponse roomDetailInfo
+        = roomService.findDetailById(roomId, authentication.userId);
+    return ResponseEntity.ok(roomDetailInfo);
+  }
 
-    @DeleteMapping("/{roomId}")
-    @ApiOperation(value = "Room 삭제", notes = "호스트는 본인의 Room을 삭제할 수 있다.")
-    public ResponseEntity<Object> delete(
-            @AuthenticationPrincipal JwtAuthentication authentication,
-            @PathVariable("roomId") Long roomId) {
+  @DeleteMapping("/{roomId}")
+  @ApiOperation(value = "Room 삭제", notes = "호스트는 본인의 Room을 삭제할 수 있다.")
+  public ResponseEntity<Object> delete(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @PathVariable("roomId") Long roomId) {
 
-        roomService.remove(roomId, authentication.userId);
-        return ResponseEntity.noContent().build();
-    }
+    roomService.remove(roomId, authentication.userId);
+    return ResponseEntity.noContent().build();
+  }
 }
