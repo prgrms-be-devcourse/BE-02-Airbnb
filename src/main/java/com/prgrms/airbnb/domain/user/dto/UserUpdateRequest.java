7e6dac22bcf3c1000e296d3103746134a4a3524f
moosongsong <songe08@gmail.com>
2022-07-06 17:19:38 +0900

    [FBNB-113] feat: 사용자 API 스웨거 적용

--- src/main/java/com/prgrms/airbnb/domain/user/dto/UserUpdateRequest.java
+++ src/main/java/com/prgrms/airbnb/domain/user/dto/UserUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.prgrms.airbnb.domain.user.dto;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,8 +9,13 @@ import lombok.Getter;
 @Getter
 @Builder
 public class UserUpdateRequest {
+  @ApiModelProperty(value = "변경하고자 하는 회원 이름", example = "abcd")
   private String name;
+
   @Email
+  @ApiModelProperty(value = "변경하고자 하는 회원 이메일", example = "gef@naver.com")
   private String email;
+
+  @ApiModelProperty(value = "변경하고자 하는 회원 전화번호", example = "010-1234-5678")
   private String phone;
 }
