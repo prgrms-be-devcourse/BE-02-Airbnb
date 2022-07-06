ffc9b3b7513d87d315ba6805bdf0a8b8bd505f1a
Hyunggeun447 <bopi447@naver.com>
2022-07-06 15:11:48 +0900

    [FBNB-116] feat: @ApiModelProperty 추가

--- src/main/java/com/prgrms/airbnb/domain/room/dto/SearchRoomRequest.java
+++ src/main/java/com/prgrms/airbnb/domain/room/dto/SearchRoomRequest.java
@@ -1,7 +1,7 @@
 package com.prgrms.airbnb.domain.room.dto;
 
-import com.prgrms.airbnb.domain.room.entity.RoomInfo;
 import com.prgrms.airbnb.domain.room.entity.RoomType;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -10,12 +10,27 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class SearchRoomRequest {
 
+  @ApiModelProperty(example = "검색어")
   private String keyword;
+
+  @ApiModelProperty(example = "주거 타입")
   private RoomType roomType;
+
+  @ApiModelProperty(example = "최대 수용 인원")
   private Integer maxGuest;
+
+  @ApiModelProperty(example = "방 개수")
   private Integer roomCount;
+
+  @ApiModelProperty(example = "침대 개수")
   private Integer bedCount;
+
+  @ApiModelProperty(example = "최소 가격(1박)")
   private Integer minCharge;
+
+  @ApiModelProperty(example = "최대 가격(1박)")
   private Integer maxCharge;
+
+  @ApiModelProperty(example = "리뷰 평점")
   private Double rating;
 }
--- src/main/java/com/prgrms/airbnb/domain/room/dto/UpdateRoomRequest.java
+++ src/main/java/com/prgrms/airbnb/domain/room/dto/UpdateRoomRequest.java
@@ -1,13 +1,13 @@
 package com.prgrms.airbnb.domain.room.dto;
 
 import com.prgrms.airbnb.domain.room.entity.RoomImage;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Builder
@@ -15,13 +15,25 @@ import java.util.List;
 @AllArgsConstructor
 public class UpdateRoomRequest {
 
+  @ApiModelProperty(example = "Room 아이디")
   private Long id;
-  private Integer charge;
-  private String name;
-  private String description;
-  private Integer maxGuest;
-  private Integer bedCount;
-  @Builder.Default
-  private List<RoomImage> roomImages = new ArrayList<>();
 
+  @ApiModelProperty(example = "가격(1박)")
+  private Integer charge;
+
+  @ApiModelProperty(example = "게시글 제목")
+  private String name;
+
+  @ApiModelProperty(example = "게시글 상세설명")
+  private String description;
+
+  @ApiModelProperty(example = "최대 수용 인원")
+  private Integer maxGuest;
+
+  @ApiModelProperty(example = "침대 개수")
+  private Integer bedCount;
+
+  @Builder.Default
+  @ApiModelProperty(example = "게시글 사진 목록")
+  private List<RoomImage> roomImages = new ArrayList<>();
 }
